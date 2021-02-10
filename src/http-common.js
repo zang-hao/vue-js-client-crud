b5c6bec3ee60c9edd4088c95383c70effd359d10
Redsheeep <qwsdcv61215@gmail.com>
2021-02-11 06:05:54 +0800

    Update TutorialsList.vue

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import Vue from "vue";
+Vue.use(axios)
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://127.0.0.1:8080/api",
   headers: {
     "Content-type": "application/json"
   }
